75036bba21658572355b052d250039512124942e
James Hughes <james@pyrosoftsolutions.co.uk>
2022-08-07 16:01:23 +0100

    --amend

--- runme.sh
+++ runme.sh
@@ -6,6 +6,5 @@ read projectName
 echo "There might be some errors listed here these are generally safe to ignore, renaming files and directories..."
 
 echo $projectName
-find . -iname "*test*" -exec rename test $projectName '{}' \;
-find . -iname "*test*" -exec rename test $projectName '{}' \;
+find . -depth -iname "*test*" -exec rename test $projectName '{}' \;
 grep -rl test . | xargs sed -i "s/test/$projectName/g"
